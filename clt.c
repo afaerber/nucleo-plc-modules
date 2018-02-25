91ee3e94329d2425e473184107ffcb4e92162e65
Andreas Färber <afaerber@suse.de>
2018-02-25 20:19:15 +0100

    Implement gpiochips for clt and vni
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- clt.c
+++ clt.c
@@ -1,10 +1,16 @@
+#include <linux/gpio.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <linux/spi/spi.h>
 
-static int clt_read(struct spi_device *spi)
+struct clt_device {
+	struct spi_device *spi;
+	struct gpio_chip gpio_chip;
+};
+
+static int clt_read(struct spi_device *spi, u8 *val)
 {
 	u8 buf[2];
 	struct spi_transfer xfer = {
@@ -17,13 +23,33 @@ static int clt_read(struct spi_device *spi)
 	if (ret < 0)
 		return ret;
 
-	dev_info(&spi->dev, "read 0x%02x 0x%02x\n", buf[0], buf[1]);
+	*val = buf[0];
 
 	return 0;
 }
 
+static int clt_get_direction(struct gpio_chip *chip, unsigned int offset)
+{
+	return GPIOF_DIR_IN;
+}
+
+static int clt_get(struct gpio_chip *chip, unsigned int offset)
+{
+	struct clt_device *data = container_of(chip, struct clt_device, gpio_chip);
+	u8 val;
+	int ret;
+
+	ret = clt_read(data->spi, &val);
+	if (ret < 0)
+		return ret;
+
+	val >>= offset;
+	return val & 0x1;
+}
+
 static int clt_probe(struct spi_device *spi)
 {
+	struct clt_device *data;
 	int ret;
 
 	spi->bits_per_word = 8;
@@ -31,17 +57,40 @@ static int clt_probe(struct spi_device *spi)
 	if (ret)
 		return ret;
 
-	ret = clt_read(spi);
-	if (ret)
-		return ret;
+	data = devm_kzalloc(&spi->dev, sizeof(*data), GFP_KERNEL);
+	if (!data)
+		return -ENOMEM;
 
-	dev_info(&spi->dev, "CLT01-38SQ7\n");
+	data->spi = spi;
+
+	data->gpio_chip.parent = &spi->dev;
+	data->gpio_chip.of_node = spi->dev.of_node;
+	data->gpio_chip.of_gpio_n_cells = 2;
+	data->gpio_chip.of_xlate = of_gpio_simple_xlate;
+	data->gpio_chip.base = -1;
+	data->gpio_chip.ngpio = 8;
+	data->gpio_chip.get_direction = clt_get_direction;
+	data->gpio_chip.get = clt_get;
+
+	ret = devm_gpiochip_add_data(&spi->dev, &data->gpio_chip, NULL);
+	if (ret) {
+		dev_err(&spi->dev, "Adding GPIO chip failed\n");
+		return ret;
+	}
+
+	spi_set_drvdata(spi, data);
+
+	dev_info(&spi->dev, "CLT01-38SQ7 probed\n");
 
 	return 0;
 }
 
 static int clt_remove(struct spi_device *spi)
 {
+	//struct clt_device *data = spi_get_drvdata(spi);
+
+	//devm_gpiochip_remove(&data->gpio_chip);
+
 	return 0;
 }
 
--- vni.c
+++ vni.c
@@ -1,33 +1,26 @@
+#include <linux/gpio.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <linux/spi/spi.h>
 
-static int vni_probe(struct spi_device *spi)
+struct vni_device {
+	struct spi_device *spi;
+	struct gpio_desc *out_en;
+	struct gpio_chip gpio_chip;
+	u8 val;
+};
+
+static int vni_send(struct spi_device *spi, u8 x)
 {
-	u8 x;
 	u8 buf[2];
 	struct spi_transfer xfer = {
 		.tx_buf = buf,
 		.len = 2,
 	};
-	int out_en;
 	int ret;
 
-	out_en = of_get_named_gpio(spi->dev.of_node, "output-enable-gpios", 0);
-	if (out_en == -ENOENT)
-		dev_warn(&spi->dev, "no OUT_EN GPIO available, ignoring\n");
-
-	if (gpio_is_valid(out_en))
-		gpio_set_value(out_en, 1);
-
-	spi->bits_per_word = 8;
-	ret = spi_setup(spi);
-	if (ret)
-		return ret;
-
-	x = 0xf0;
 	buf[0] = x;
 	buf[1] = ((((x >> 0) & 0x1) +
 		   ((x >> 1) & 0x1) +
@@ -48,6 +41,71 @@ static int vni_probe(struct spi_device *spi)
 		    ((x >> 6) & 0x1)) & 0x1) << 3;
 
 	ret = spi_sync_transfer(spi, &xfer, 1);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static int vni_get_direction(struct gpio_chip *chip, unsigned int offset)
+{
+	return GPIOF_DIR_OUT;
+}
+
+static void vni_set(struct gpio_chip *chip, unsigned int offset, int value)
+{
+	struct vni_device *data = container_of(chip, struct vni_device, gpio_chip);
+
+	if (value)
+		data->val |= BIT(offset);
+	else
+		data->val &= ~BIT(offset);
+
+	vni_send(data->spi, data->val);
+}
+
+static int vni_probe(struct spi_device *spi)
+{
+	struct vni_device *data;
+	int ret;
+
+	data = devm_kzalloc(&spi->dev, sizeof(*data), GFP_KERNEL);
+	if (!data)
+		return -ENOMEM;
+
+	data->spi = spi;
+
+	data->out_en = devm_gpiod_get_optional(&spi->dev, "output-enable",
+				GPIOD_OUT_HIGH);
+	if (!data->out_en)
+		dev_warn(&spi->dev, "no OUT_EN GPIO available, ignoring\n");
+
+	if (data->out_en && !IS_ERR(data->out_en))
+		gpiod_set_value(data->out_en, 1);
+
+	spi->bits_per_word = 8;
+	ret = spi_setup(spi);
+	if (ret)
+		return ret;
+
+	data->gpio_chip.parent = &spi->dev;
+	data->gpio_chip.of_node = spi->dev.of_node;
+	data->gpio_chip.of_gpio_n_cells = 2;
+	data->gpio_chip.of_xlate = of_gpio_simple_xlate;
+	data->gpio_chip.base = -1;
+	data->gpio_chip.ngpio = 8;
+	data->gpio_chip.get_direction = vni_get_direction;
+	data->gpio_chip.set = vni_set;
+
+	data->val = 0x0;
+
+	ret = devm_gpiochip_add_data(&spi->dev, &data->gpio_chip, NULL);
+	if (ret) {
+		dev_err(&spi->dev, "Adding GPIO chip failed\n");
+		return ret;
+	}
+
+	ret = vni_send(spi, data->val);
 	if (ret < 0)
 		return ret;
 
